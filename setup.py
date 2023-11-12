0581d7fe3577e8709b14f8a348904ad11497f9bc
Heiner Lohaus <hlohaus@users.noreply.github.com>
2023-11-12 05:33:20 +0100

    Update pip

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python
-
 from setuptools import find_packages, setup
+
+with open('README.md') as fh:
+    long_description = fh.read()
 
 if __name__ == "__main__":
     setup(
         name='py_arkose_generator',
+        version='0.0.0.2',
         packages=find_packages(),
-        install_requires=["mmh3"],
+        install_requires=['pycryptodome', 'mmh3'],
+        long_description_content_type='text/markdown',
+        long_description=long_description,
+        url='https://github.com/Zai-Kun/py-arkose-token-generator/tree/main',
     )
